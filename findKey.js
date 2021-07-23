a849af38887ead4192608fe55cd46360f473dbe6
Lucia <glutnik.lucia@gmail.com>
2021-07-22 22:35:55 -0700

    findKey

---
+++ findKey.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  let result;
+  let emo;
+  let str;
+  if (actual === expected) {
+    result = '===';
+    emo = '✅✅✅';
+    str = 'Passed';
+  } else {
+    result = '!==';
+    emo = '🛑🛑🛑';
+    str = 'Failed';
+  }
+  
+  console.log(`${emo} Assertion ${str}: ${actual} ${result} ${expected}`);
+};
+
+const findKey = function(obj, callback) {
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma');// => "noma"
