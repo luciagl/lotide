274b9174e71438ab2cd95751d2dca30782407065
Lucia <glutnik.lucia@gmail.com>
2021-07-26 23:24:25 -0700

    Update: using Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,11 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+//const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1, 2, 3]), [2]); 
+describe("#head", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });  
+});
+
+//assertArraysEqual(middle([1, 2, 3]), [2]); 
